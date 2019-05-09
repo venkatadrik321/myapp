35b630af1d2e48ac440a1a0ee721489f355dca40
sathya <sathya@sathya-PC>
2019-05-09 12:46:19 +0530

    SID-88956 New update

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -2,20 +2,24 @@ package com.app;
 
 public class Employee {
 
-	private int empId;
+	
+	
+	private String empName;
 
-	public int getEmpId() {
-		return empId;
+	public String getEmpName() {
+		return empName;
 	}
 
-	public void setEmpId(int empId) {
-		this.empId = empId;
+	public void setEmpName(String empName) {
+		this.empName = empName;
 	}
 
 	@Override
 	public String toString() {
-		return "Employee [empId=" + empId + "]";
+		return "Employee [empName=" + empName + "]";
 	}
+	
+	
 	
 	
 	
